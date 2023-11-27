97d8f60fc98ee7600b70c0c69e115b88abf81f13
talha <talhaaly103@gmail.com>
2023-11-27 10:56:34 +0500

    PJt

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+
+// navBar 
+
+hamburger = document.querySelector(".hamburger");
+hamburger.onclick = function() {
+    navBar = document.querySelector(".nav-bar");
+    navBar.classList.toggle("active");
+}
+
+
+
 const UserName = document.getElementById('UserName')
 const Email = document.getElementById('Email')
 const Password = document.getElementById('Password')
@@ -15,12 +26,7 @@ const signUpHandler = () => {
     if(!UserName || !Email || !Password || !CPassword ) return alert ('Sari field likh bhai')
     if(Password.ariaValueMax.length >=8) return alert ('pory length ka password likh bhai')
     if(Password.value != CPassword.value) return alert('Same password  likh  bhai')
-    const user = {
-UserName = UserName.value,
-Email = Email.value,
-Password = Password.value,
-Password = CPassword.value
-}
+
 alert("signUp SuccessFully, now you can login")
 setTimeout(()=>{
     window.location.href = '../login.login.html'
